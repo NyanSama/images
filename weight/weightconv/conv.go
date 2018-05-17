8c1887bf4ecb2c95db8667fe8e7c4efae62ae5de
nyansama <nyansamsa@163.com>
2018-05-17 17:57:46 +0800

    add comment

--- weight/weightconv/conv.go
+++ weight/weightconv/conv.go
@@ -1,4 +1,7 @@
 package weightconv
 
+//IToM function fo feet to meter
 func IToM(i Inch) Mili { return Mili(i * 0.3048) }
+
+//MToI function for meter to feet
 func MToI(m Mili) Inch { return Inch(m * 3.2808399) }
